9e4d27526eeef58ce3eea127ad4d9830f90b9b72
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-03-16 16:41:43 +0100

    ressoruce route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MouvementController;
+use App\Http\Controllers\RessourceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::apiResource('/mouvement', MouvementController::class);
+Route::apiResource('/ressource', RessourceController::class);
