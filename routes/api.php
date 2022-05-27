ea9bcbd4755e45303ef11142e0a9486066ef9271
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-05-27 14:09:05 +0200

    add ressourceApi route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MouvementController;
 use App\Http\Controllers\RessourceController;
+use App\Http\Controllers\TypeRessourceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/mouvement', MouvementController::class);
 Route::apiResource('/ressource', RessourceController::class);
+Route::apiResource('/typeressource', TypeRessourceController::class);
 Route::get('/ressource/user/{id}', [RessourceController::class, 'showperuser']);
 Route::get('/ressourcetotal/user/{id}', [MouvementController::class, 'TotalEntreSortie']);
