08e16d975363e5a754f1b6d9841869bace9e339a
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-20 17:32:58 +0200

    setup authentification controller

---
+++ app/Http/Controllers/AuthentificationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthentificationController extends Controller
+{
+    public function create(Request $request)
+    {
+        # code...
+    }
+
+    public function login(Request $request)
+    {
+        # code...
+    }
+
+    public function logout(Request $request)
+    {
+        # code...
+    }
+}
