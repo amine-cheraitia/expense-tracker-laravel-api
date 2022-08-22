6e5c4dcec5c08097e59ce6aa23a9ea76971c12eb
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-08-22 02:03:57 +0200

    setup middleware to routes

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/mouvement', MouvementController::class);
-Route::apiResource('/ressource', RessourceController::class);
-Route::apiResource('/typeressource', TypeRessourceController::class);
-Route::get('/ressource/user/{id}', [RessourceController::class, 'showperuser']);
-Route::get('/ressourcetotal/user/{id}', [MouvementController::class, 'TotalEntreSortie']);
+Route::apiResource('/mouvement', MouvementController::class)->middleware('auth:sanctum');
+Route::apiResource('/ressource', RessourceController::class)->middleware('auth:sanctum');
+Route::apiResource('/typeressource', TypeRessourceController::class)->middleware('auth:sanctum');
+Route::get('/ressource/user/{id}', [RessourceController::class, 'showperuser'])->middleware('auth:sanctum');
+Route::get('/ressourcetotal/user/{id}', [MouvementController::class, 'TotalEntreSortie'])->middleware('auth:sanctum');;
 
 Route::post('/login', [AuthentificationController::class, 'login']);
 Route::post('/logout', [AuthentificationController::class, 'logout']);
 Route::post('/create', [AuthentificationController::class, 'create']);
+Route::post('/checktoken', [AuthentificationController::class, 'checkToken'])->middleware('auth:sanctum');;
